ac9b3b3c3ddae6df529fc1458407c784d1bdebf9
Sketch94 <75249620+Sketch94@users.noreply.github.com>
2025-01-21 17:27:33 +0100

    Add files via upload

---
+++ SimpleStruct/Program.cs
@@ -0,0 +1,29 @@
+﻿public struct MyStruct
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public MyStruct(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public MyStruct WithX(int x) => new MyStruct(x, Y);
+    public MyStruct WithY(int y) => new MyStruct(X, y);
+
+    public override string ToString() => $"({X}, {Y})";
+
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var s = new MyStruct(1, 2);
+        Console.WriteLine(s);
+        Console.WriteLine(s.WithX(3));
+        Console.WriteLine(s.WithY(4));
+    }
+}
+
